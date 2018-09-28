27bdb3b7c005d8a91de4eb54612f172ed5a50c20
Ritz <ritesh.kumar2905@gmail.com>
2018-09-28 14:58:13 -0400

    changing 02-Tuple

--- 02-Tuple.cpp
+++ 02-Tuple.cpp
@@ -7,12 +7,23 @@ int main()
 {
 	//declaring tuple
 	tuple<int,int,int> t1;
+	tuple<int,string,int> t2;
 
 	//adding values to tuple
 	t1 = make_tuple(9,2,11);
+	t2 = make_tuple(2," is for ",9);
 
 	//printing tuple
-	cout<<t1.first<<t1.second<<t1.third;
+	cout<<get<0>(t1)<<"\t"<<get<1>(t1)<<"\t"<<get<2>(t1)<<"\n";
+
+	int a,b;
+	string s;
+
+	//using tie
+	tie(a,s,b) = t2;
+
+	//printing
+	cout<<a<<s<<b<<endl;
 
 	return 0;
 }
